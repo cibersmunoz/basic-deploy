2c767b6133c501518c7c532f641bbc2ca924ad43
cibersmunoz <salvamj.it@gmail.com>
2025-03-07 13:19:50 -0500

    Basic Deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+apt update
+apt install nodejs npm -y
+npm install -g pm2
+
+git clone a tu repo
+
+cd a tu repo
+
+npm i
+
+node server.js
+
+pm2 start server.js --name maravillosa-web
+
+pm2 stop maravillosa-web
+
+pm2 delete maravillosa-web
